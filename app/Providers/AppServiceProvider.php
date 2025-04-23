f1e8ded9adf50500ea009a0b6280b189a139eacc
Charles Marnie B. Limpo <charlesmarnielimpo@gmail.com>
2025-04-23 08:32:13 +0800

    chore: initial project setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureVite();
+        $this->configureDates();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+}
