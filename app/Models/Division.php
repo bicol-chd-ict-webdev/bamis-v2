34a1cf2400e628ee875c7535cc6170d53696c37f
Charles Marnie B. Limpo <charlesmarnielimpo@gmail.com>
2025-04-25 09:50:36 +0800

    chore: define `hasMany` relationship between Section and Division models

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
@@ -14,6 +15,14 @@ class Division extends Model
     use SoftDeletes;
 
     protected $fillable = ['name', 'acronym'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Section, covariant $this>
+     */
+    public function sections(): HasMany
+    {
+        return $this->hasMany(Section::class);
+    }
 
     /**
      * @return Attribute<mixed, mixed>
