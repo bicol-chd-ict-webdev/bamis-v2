ac5ba80bf0b8d35fef21e0822108c2baca39d311
Charles Marnie B. Limpo <charlesmarnielimpo@gmail.com>
2025-04-25 15:48:49 +0800

    feat(admin/account): add `StoreAccountRequest` and `UpdateAccountRequest` classes for validation

---
+++ app/Http/Requests/Admin/Account/UpdateAccountRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Admin\Account;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateAccountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $user = auth()->user();
+
+        return $user && $user->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:300'],
+            'email' => ['required', 'email', Rule::unique('users')->ignore($this->route('account'))],
+            'designation' => ['required', 'string', 'max:150'],
+            'status' => ['required', 'string', 'in:Active,Inactive'],
+            'role' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * @return array<string, string|array<string, string>>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'The :attribute field is required.',
+            'string' => 'The :attribute must be a string.',
+            'max' => 'The :attribute may not be greater than :max characters.',
+            'email' => 'The :attribute must be a valid email address.',
+            'unique' => 'The :attribute has already been taken.',
+            'in' => 'The selected :attribute is invalid.',
+        ];
+    }
+}
